bf893673ad8b0377641c98f450f021e4691e86b8
Jamie Hou <jamiehou@whaugmentum.com.cn>
2015-01-21 17:40:07 +0800

    Add demo project ControlFun

---
+++ ControlFun/ControlFun/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ControlFun
+//
+//  Created by MAC Mini M on 1/21/15.
+//  Copyright (c) 2015 ioryz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
