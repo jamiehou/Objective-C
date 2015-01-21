7a0a4b0ccbb3971d4760edd1b40cbf979f18a8e5
jamiehou <ioryz198510@163.com>
2015-01-22 01:03:03 +0800

    Finished demo ControlFun

--- ControlFun/ControlFun/ViewController.h
+++ ControlFun/ControlFun/ViewController.h
@@ -8,8 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIActionSheetDelegate>
 
+@property (weak, nonatomic) IBOutlet UITextField *tfName;
+@property (weak, nonatomic) IBOutlet UITextField *tfNumber;
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
+@property (weak, nonatomic) IBOutlet UISwitch *switchLeft;
+@property (weak, nonatomic) IBOutlet UISwitch *switchRight;
+@property (weak, nonatomic) IBOutlet UIButton *btnDoSth;
 
+- (IBAction)tfDoneEdit:(id)sender;
+- (IBAction)bgTap:(id)sender;
+- (IBAction)silderChange:(id)sender;
+- (IBAction)switchChanged:(id)sender;
+- (IBAction)buttonPressed:(id)sender;
+- (IBAction)toggleControls:(id)sender;
 @end
 
--- ControlFun/ControlFun/ViewController.m
+++ ControlFun/ControlFun/ViewController.m
@@ -24,4 +24,55 @@
   // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)tfDoneEdit:(id)sender {
+  [sender resignFirstResponder];
+}
+
+- (IBAction)bgTap:(id)sender {
+  [_tfName resignFirstResponder];
+  [_tfNumber resignFirstResponder];
+}
+
+- (IBAction)silderChange:(id)sender {
+  UISlider *slider = (UISlider *)sender;
+  int sliderValue = (int)roundf(slider.value);
+  [_sliderLabel setText:[NSString stringWithFormat:@"%d", sliderValue]];
+}
+
+- (IBAction)switchChanged:(id)sender {
+  UISwitch *whichSwitch = (UISwitch *)sender;
+  BOOL switchValue = whichSwitch.isOn;
+  [_switchLeft setOn:switchValue animated:YES];
+  [_switchRight setOn:switchValue animated:YES];
+}
+
+- (IBAction)buttonPressed:(id)sender {
+  UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Are you sure?" delegate:self cancelButtonTitle:@"No!" destructiveButtonTitle:@"Yes, sure!" otherButtonTitles:nil];
+  [actionSheet showInView:self.view];
+}
+
+- (IBAction)toggleControls:(id)sender {
+  if ([sender selectedSegmentIndex] == 0) {
+    _switchLeft.hidden = NO;
+    _switchRight.hidden = NO;
+    _btnDoSth.hidden = YES;
+  } else {
+    _switchLeft.hidden = YES;
+    _switchRight.hidden = YES;
+    _btnDoSth.hidden = NO;
+  }
+}
+
+-(void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex {
+  if (buttonIndex != [actionSheet cancelButtonIndex]) {
+    NSString *msg = nil;
+    if (_tfName.text.length > 0) {
+      msg = [NSString stringWithFormat:@"You have finished doing something, %@", _tfName.text];
+    } else {
+      msg = @"You have finished doing something.";
+    }
+    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Someting was done" message:msg delegate:nil cancelButtonTitle:@"Gun!" otherButtonTitles:nil];
+    [alert show];
+  }
+}
 @end
