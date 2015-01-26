c787614e907512a554ae436ed0ddb07469fe9ce4
jamiehou <ioryz198510@163.com>
2015-01-26 23:04:33 +0800

    Refined simple table view demo

--- TableFun/TableFun/ViewController.m
+++ TableFun/TableFun/ViewController.m
@@ -33,11 +33,50 @@
   static NSString *simpleTableCellIdentifier = @"SimpleTableCellIdentifier";
   UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableCellIdentifier];
   if (cell == nil) {
-    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableCellIdentifier];
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:simpleTableCellIdentifier];
   }
+  UIImage *image = [UIImage imageNamed:@"star.png"];
+  cell.imageView.image = image;
+  UIImage *imageHighlighted =  [UIImage imageNamed:@"star2.png"];
+  cell.imageView.highlightedImage = imageHighlighted;
   NSInteger row = [indexPath row];
   cell.textLabel.text = [_listData objectAtIndex:row];
+  cell.textLabel.font = [UIFont boldSystemFontOfSize:30];
+  if (row < 7) {
+    cell.detailTextLabel.text = @"Mr. Jamie";
+  } else {
+    cell.detailTextLabel.text = @"Mrs. ZLL";
+  }
   return cell;
 }
 
+- (NSInteger)tableView:(UITableView *)tableView indentationLevelForRowAtIndexPath:(NSIndexPath *)indexPath {
+  NSInteger r = [indexPath row];
+  int s = r % 2;
+  if (s == 0) {
+    return 0;
+  } else {
+    return 4;
+  }
+}
+
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+  NSInteger row = [indexPath row];
+  if (row == 0) {
+    return nil;
+  }
+  return indexPath;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+  NSString *selected = [_listData objectAtIndex:[indexPath row]];
+  NSString *msg = [NSString stringWithFormat:@"You selected %@", selected];
+  UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Select Cell" message:msg delegate:nil cancelButtonTitle:@"OK, I know" otherButtonTitles:nil];
+  [tableView deselectRowAtIndexPath:indexPath animated:YES];
+  [alert show];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+  return 100;
+}
 @end
